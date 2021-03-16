d6d5286a1133917d7d8c6e09b0a94137625d93a6
Carlos E. La Torre <amautacelt@gmail.com>
2021-03-16 19:23:37 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   resources :students, only: :index
 
   # get "students", to: "students#index"
-  get '/students', to: "students#index"
+  # get '/students', to: "students#index"
+  resources :students, only: :index
   get '/students/:id', to: "students#show"
 end
